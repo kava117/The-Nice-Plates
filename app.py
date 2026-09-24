819de37de45a6f25835c23f3191fe730e813a69c
ZSRoach <zachary@okmaybe.com>
2026-09-23 23:02:28 -0400

    Added routes for testing database connectivity
    Added a route to add a single test user (repeats static values)
    Added a route to fetch the users table as raw json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, g
+from flask import Flask, g, jsonify
 import sqlite3
 from database import database
 
@@ -17,6 +17,17 @@ def access_db():
 @app.route("/")
 def test():
     return "<p>Wow we tested<p>"
+
+@app.route("/add-user")
+def addUserRoute():
+    testName = "Laura Bailey"
+    testEmail = "lbailey4022@yahoo.com"
+    database.addUser(testName,testEmail)
+    return "<p>New user added to the database!<p>"
+
+@app.route("/fetch-users")
+def fetchUsersRoute():
+    return jsonify(database.fetchUsers())
 
 if __name__ == "__main__":
     app.run(port=portNum)
